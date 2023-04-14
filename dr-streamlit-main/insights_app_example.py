02f0e3dc95fe972be4657655b76b446f3fc947b0
ina-ko <ina@datarobot.com>
2023-04-14 15:04:57 -0400

    Update insights_app_example.py
    # to hide the multiclass code lines.

--- dr-streamlit-main/insights_app_example.py
+++ dr-streamlit-main/insights_app_example.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
 
     project_id = os.getenv('projectid')
     model_id = project_model_dropdown(project_id)
-    selected_class = multiclass_dropdown_menu(project_id, model_id)
+    #selected_class = multiclass_dropdown_menu(project_id, model_id)
     selected_feature = text_feature_dropdown_menu(project_id)
     wordcloud_chart(
         project_id=project_id,
         model_id=model_id,
-        specified_class=selected_class,
+        #specified_class=selected_class,
         selected_feature=selected_feature,
     )
     derived_features_chart(
         project_id=project_id,
         model_id=model_id,
-        selected_class=selected_class
+        #selected_class=selected_class
     )
