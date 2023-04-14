c3f5f007dcb3693ca67773a28fcf3715dfbf2a53
ina-ko <ina@datarobot.com>
2023-04-14 14:15:34 -0400

    Update insights_app_example.py

--- dr-streamlit-main/insights_app_example.py
+++ dr-streamlit-main/insights_app_example.py
@@ -1,3 +1,4 @@
+import os
 from datarobot import Client
 from datarobot.client import set_client
 from dr_streamlit import multiclass_dropdown_menu, derived_features_chart, text_feature_dropdown_menu, project_model_dropdown, wordcloud_chart
@@ -5,12 +6,12 @@ from dr_streamlit import multiclass_dropdown_menu, derived_features_chart, text_
 
 if __name__ == '__main__':
     c = Client(
-        token='',
+        token=os.getenv('token'),
         endpoint='https://app.datarobot.com/api/v2/'
     )
     set_client(c)
 
-    project_id = ""
+    project_id = os.getenv('projectid')
     model_id = project_model_dropdown(project_id)
     selected_class = multiclass_dropdown_menu(project_id, model_id)
     selected_feature = text_feature_dropdown_menu(project_id)
